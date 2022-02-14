7580600bbd32266aab69b7551df0c0beee5c520c
William Tayeb <tayebwilliam@gmail.com>
2022-02-14 15:38:15 +0100

    Configure splash screen for Android

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
+import SplashScreen from "react-native-splash-screen";
 
 import { Login } from 'screens';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View>
       <Text>ChatApp</Text>
